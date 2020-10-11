6c98b329726ead77fb2b54577b42b71afd590105
user <user@Home-PC>
2020-10-11 12:03:50 +0300

    codeTest

--- GitHubConecttionTest/src/test/TestConnection.java
+++ GitHubConecttionTest/src/test/TestConnection.java
@@ -3,7 +3,10 @@ package test;
 public class TestConnection {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		for (int i = 0; i < 10; i++) {
+			System.out.println(i);
+			
+		}
 
 	}
 
